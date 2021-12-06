778275858849e6b62d1e3d8df708a69debc9ddbd
Arun Yokesh <ayokesh.cse@gmail.com>
2021-12-06 23:01:10 +0530

    server clean up

--- server/bin/index.js
+++ server/bin/index.js
@@ -1,13 +1,10 @@
 #!/usr/bin/env node
-
-const colors = require("colors");
-const { exec, execSync } = require("child_process");
-const { cleanup } = require("../utils/helper");
+require('colors');
+const { execSync } = require("child_process");
 const { initializeStaticRoutes } = require("../static-files");
+const { appRoutes } = require("../routes");
 
 const currentFolder = process.cwd();
-
-const ChromeLauncher = require("chrome-launcher");
 
 const getPackage = (res) => {
   var fs = require("fs"),
@@ -32,150 +29,29 @@ const getPackage = (res) => {
   });
 };
 
-var express = require("express");
-var app = express();
-initializeStaticRoutes(app);
-
-// Add headers before the routes are defined
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3663");
-
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-
-  // Request headers you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true);
-
-  // Pass to next layer of middleware
-  next();
-});
-
-app.get("/", function (req, res) {
-  res.send("Welcome to remallow!");
-});
-
-app.get("/packages", function (req, res) {
-  getPackage(res);
-});
-
-app.get("/package/install", function (req, res) {
-  const {
-    query: { manager, dependency, packageName },
-  } = req;
-
-  const resultKeyword = cleanup(packageName);
-
-  console.log("Installing... ".green + resultKeyword);
-  console.log(`${manager} install ${packageName} ${dependency}`.green);
-
-  exec(
-    `${manager} install ${packageName} ${dependency}`,
-    (error, stdout, stderr) => {
-      if (error) {
-        console.log(`error: ${error.message}`.red);
-        res.status(500).send({
-          success: false,
-          message: `error: ${error.message}`,
-        });
-        return;
-      }
-      if (stderr) {
-        console.log(`stderr: ${stderr}`.red);
-        res.status(500).send({
-          success: false,
-          message: `stderr: ${stderr}`,
-        });
-        return;
-      }
-      console.log(`stdout: ${stdout}`);
-      res.status(200).send({
-        success: true,
-      });
-    }
-  );
-});
-
-app.get("/package/uninstall", function (req, res) {
-  console.log("Removing... ".green + req.query.packageName);
-
-  exec(`npm uninstall ${req.query.packageName}`, (error, stdout, stderr) => {
-    if (error) {
-      console.log(`error: ${error.message}`.red);
-      res.status(500).send({
-        success: false,
-        message: `error: ${error.message}`,
-      });
-      return;
-    }
-    if (stderr) {
-      console.log(`stderr: ${stderr}`.red);
-      res.status(500).send({
-        success: false,
-        message: `stderr: ${stderr}`,
-      });
-      return;
-    }
-    console.log(`stdout: ${stdout}`);
-    res.status(200).send({
-      success: true,
+(function init() {
+  try {
+    const ChromeLauncher = require("chrome-launcher");
+    var express = require("express");
+    var app = express();
+    initializeStaticRoutes(app);
+    appRoutes({
+      app,
+      getPackage,
     });
-  });
-});
 
-app.get("/package/search", function (req, res) {
-  const {
-    query: { keyword },
-  } = req;
-  // let removeElements = ["npm","yarn", "--save", "--save-dev", " install ", " add "];
-  const resultKeyword = cleanup(keyword);
-
-  console.log("Searching... ".green + resultKeyword);
-
-  exec(`npm search ${resultKeyword} --json`, (error, stdout, stderr) => {
-    if (error) {
-      console.log(`error: ${error.message}`.red);
-      res.status(500).send({
-        success: false,
-        message: `error: ${error.message}`,
-      });
-      return;
-    }
-    if (stderr) {
-      console.log(`stderr: ${stderr}`.red);
-      res.status(500).send({
-        success: false,
-        message: `stderr: ${stderr}`,
-      });
-      return;
-    }
-    console.log(`stdout: ${stdout}`);
-    res.status(200).send({
-      success: true,
-      json: JSON.parse(stdout),
-      keyword: resultKeyword,
+    app.listen(8081, function () {
+      var host = "http://127.0.0.1";
+      var port = "8081";
+      console.log("listening at %s:%s".magenta, host, port);
     });
-  });
-});
 
-app.listen(8081, function () {
-  var host = "http://127.0.0.1";
-  var port = "8081";
-  console.log("Remallow app listening at %s:%s".magenta, host, port);
-});
-
-ChromeLauncher.launch({
-  startingUrl: "http://127.0.0.1:8081",
-}).then((chrome) => {
-  console.log(`Remallow Package Manager running on http://127.0.0.1:8081`);
-});
+    ChromeLauncher.launch({
+      startingUrl: "http://127.0.0.1:8081",
+    }).then((chrome) => {
+      console.log(`Remallow Package Manager running on http://127.0.0.1:8081`.green);
+    });
+  } catch (e) {
+    consola.error(e);
+  }
+})();
---
+++ server/routes.js
@@ -0,0 +1,140 @@
+const { exec } = require("child_process");
+const { cleanup } = require("./utils/helper");
+
+function appRoutes({app, getPackage}) {
+  // Add headers before the routes are defined
+  app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3663");
+
+    // Request methods you wish to allow
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
+
+    // Request headers you wish to allow
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "X-Requested-With,content-type"
+    );
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader("Access-Control-Allow-Credentials", true);
+
+    // Pass to next layer of middleware
+    next();
+  });
+
+  app.get("/", function (req, res) {
+    res.send("Welcome to remallow!");
+  });
+
+  app.get("/packages", function (req, res) {
+    getPackage(res);
+  });
+
+  app.get("/package/install", function (req, res) {
+    const {
+      query: { manager, dependency, packageName },
+    } = req;
+
+    const resultKeyword = cleanup(packageName);
+
+    console.log("Installing... ".green + resultKeyword);
+    console.log(`${manager} install ${packageName} ${dependency}`.green);
+
+    exec(
+      `${manager} install ${packageName} ${dependency}`,
+      (error, stdout, stderr) => {
+        if (error) {
+          console.log(`error: ${error.message}`.red);
+          res.status(500).send({
+            success: false,
+            message: `error: ${error.message}`,
+          });
+          return;
+        }
+        if (stderr) {
+          console.log(`stderr: ${stderr}`.red);
+          res.status(500).send({
+            success: false,
+            message: `stderr: ${stderr}`,
+          });
+          return;
+        }
+        console.log(`stdout: ${stdout}`);
+        res.status(200).send({
+          success: true,
+        });
+      }
+    );
+  });
+
+  app.get("/package/uninstall", function (req, res) {
+    console.log("Removing... ".green + req.query.packageName);
+
+    exec(`npm uninstall ${req.query.packageName}`, (error, stdout, stderr) => {
+      if (error) {
+        console.log(`error: ${error.message}`.red);
+        res.status(500).send({
+          success: false,
+          message: `error: ${error.message}`,
+        });
+        return;
+      }
+      if (stderr) {
+        console.log(`stderr: ${stderr}`.red);
+        res.status(500).send({
+          success: false,
+          message: `stderr: ${stderr}`,
+        });
+        return;
+      }
+      console.log(`stdout: ${stdout}`);
+      res.status(200).send({
+        success: true,
+      });
+    });
+  });
+
+  app.get("/package/search", function (req, res) {
+    const {
+      query: { keyword },
+    } = req;
+    // let removeElements = ["npm","yarn", "--save", "--save-dev", " install ", " add "];
+    const resultKeyword = cleanup(keyword);
+
+    console.log("Searching... ".green + resultKeyword);
+
+    exec(`npm search ${resultKeyword} --json`, (error, stdout, stderr) => {
+      if (error) {
+        console.log(`error: ${error.message}`.red);
+        res.status(500).send({
+          success: false,
+          message: `error: ${error.message}`,
+        });
+        return;
+      }
+      if (stderr) {
+        console.log(`stderr: ${stderr}`.red);
+        res.status(500).send({
+          success: false,
+          message: `stderr: ${stderr}`,
+        });
+        return;
+      }
+      console.log(`stdout: ${stdout}`);
+      res.status(200).send({
+        success: true,
+        json: JSON.parse(stdout),
+        keyword: resultKeyword,
+      });
+    });
+  });
+}
+
+module.exports = {
+    appRoutes
+}
